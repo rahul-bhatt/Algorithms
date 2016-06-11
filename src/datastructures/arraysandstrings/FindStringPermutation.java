7c7825c0d20faa7750c3e5883a602dff1ba72535
rahul-bhatt <rbhatt.27@gmail.com>
2016-06-11 08:33:46 +0530

    String permutation problem.

--- src/datastructures/arraysandstrings/FindStringPermutation.java
+++ src/datastructures/arraysandstrings/FindStringPermutation.java
@@ -4,17 +4,57 @@
 package datastructures.arraysandstrings;
 
 /**
+ * Given two Strings, implement an algorithm to determine if
+ * one is a permutation of other.
+ * 
  * @author Rahul
  *
  */
 public class FindStringPermutation {
-
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(isPermutation("abcd", "dcba"));
+		System.out.println(isPermutationUsingCharacterCounts("abcd", "dcba"));
+		
+		System.out.println(isPermutation("abcd", "dcca"));
+		System.out.println(isPermutationUsingCharacterCounts("abcd", "dcca"));
+	}
+	
+	/*
+	 * First algorithm: Sort the string and compare the results.
+	 */
+	public static boolean isPermutation(String s, String t) {
+		//First optimization: If Strings are of different length then return false.
+		if(s.length() != t.length()) return false;
+		
+		//compare the sorted Strings.
+		return sort(s).equals(sort(t));
+	}
+	
+	private static String sort(String s) {
+		char[] charSet = s.toCharArray();
+		java.util.Arrays.sort(charSet);
+		return new String(charSet);
+	}
+	
+	/*
+	 * Second algorithm: Compare the character counts.
+	 */
+	public static boolean isPermutationUsingCharacterCounts(String s, String t) {
+		//First optimization: If Strings are of different length then return false.
+		if(s.length() != t.length()) return false;
+		
+		//Assumption: ASCII char set.
+		int[] charCount = new int[256];
+		for(int i = 0; i < s.length(); i++) {
+			charCount[s.charAt(i)]++;
+		}
+		
+		for(int i = 0; i < t.length(); i++) {
+			if(--charCount[t.charAt(i)] < 0) return false;
+		}
+		
+		return true;
 	}
 
 }
