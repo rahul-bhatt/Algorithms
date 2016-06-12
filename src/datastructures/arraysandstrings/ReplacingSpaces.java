d297a6f10c1294d63f44247d14f012c4b5a840fe
rahul-bhatt <rbhatt.27@gmail.com>
2016-06-12 09:06:26 +0530

    Replacing string problem.

--- src/datastructures/arraysandstrings/ReplacingSpaces.java
+++ src/datastructures/arraysandstrings/ReplacingSpaces.java
@@ -7,19 +7,54 @@ package datastructures.arraysandstrings;
  * Implement an algorithm to replace all spaces in a string with "%20".
  * You may assume that the String has sufficient space at the end of the String to hold the additional
  * characters and that you are given the true length of the String.
- * In Java use a charcter array to perform the operation in place.
+ * In Java use a character array to perform the operation in place.
+ * 
+ * Input: "Mr John Smith      ", 13
+ * Output: "Mr%20John%20Smith"
  * 
  * @author Rahul
  *
  */
 public class ReplacingSpaces {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str = new String("John Smith      ");
+		char[] charArray = str.toCharArray();
+		
+		replaceSpaces(charArray, 10);
+		
+		for(int i = 0; charArray[i] != '\0'; i++) {
+			System.out.print(charArray[i]);
+		}
 	}
-
+	
+	/* The algorithm works through a two scan approach.
+	 * In the first scan, we count how many spaces are there in the string.
+	 * This is used to compute how long the final string should be.
+	 * In the second pass we actually edit the string.
+	 */
+	public static void replaceSpaces(char[] charArray, int length) {
+		
+		int noOfSpaces = 0, newLength = 0;
+		
+		//First count the number of spaces to calculate final length of the string.
+		for(int i = 0; i < length; i++) {
+			if(charArray[i] == ' ') noOfSpaces++;
+		}
+		
+		newLength = length + noOfSpaces * 2;
+		charArray[newLength] = '\0';
+		
+		//Move backwards and edit the string so that we don't need to worry about
+		//overwriting characters.
+		while(length >= 0 && length != newLength) {
+			if(charArray[--length] != ' ') {
+				charArray[--newLength] = charArray[length];
+			} else {
+				charArray[--newLength] = '0';
+				charArray[--newLength] = '2';
+				charArray[--newLength] = '%';
+			}
+		}
+	}
 }
