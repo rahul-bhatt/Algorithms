4aefaeaf34c3fc97d1b3b5aac2b8a23879cb2a80
rahul-bhatt <rbhatt.27@gmail.com>
2016-06-18 20:47:42 +0530

    String compression problem.

--- src/datastructures/arraysandstrings/StringCompression.java
+++ src/datastructures/arraysandstrings/StringCompression.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package datastructures.arraysandstrings;
 
 /**
@@ -14,12 +11,68 @@ package datastructures.arraysandstrings;
  */
 public class StringCompression {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Compressed string for aabcccccaaa: " + compressString("aabcccccaaa"));
+		System.out.println("Compressed string for abc: " + compressString("abc"));
+	}
 
+	/*
+	 * Algorithm: Scan through the keeping track of previous character and counts.
+	 * Use StringBuffer for improved performance, otherwise String concatenation would an
+	 * expensive operation (O(n*2)).
+	 */
+	private static String compressString(String str) {
+		
+		if(str.length() < countCompressedString(str)) {
+			return str;
+		}
+		
+		StringBuffer compressedString = new StringBuffer();
+		
+		char prevChar = str.charAt(0);
+		int charCount = 1;
+		compressedString.append(prevChar);
+		
+		for(int i = 1; i < str.length(); i++) {
+			char chr = str.charAt(i);
+			
+			if(prevChar == chr) {
+				charCount++;
+			} else {
+				compressedString.append(charCount);
+				compressedString.append(chr);
+				charCount = 1;
+				prevChar = chr;
+			}
+		}
+		
+		compressedString.append(charCount);
+		
+		return compressedString.toString();
+	}
+	
+	/*
+	 * Count the size of the compressed string vs prgional string.
+	 */
+	private static int countCompressedString(String str) {
+		if(str == null || str.isEmpty()) return 0;
+		
+		char prevChar = str.charAt(0);
+		int size = 0;
+		int count = 1;
+		
+		for(int i = 1; i < str.length(); i++) {
+			if(str.charAt(i) == prevChar) {
+				count++;
+			} else {
+				prevChar = str.charAt(i);
+				size += 1 + String.valueOf(count).length();
+				count = 1;
+			}
+		}
+		
+		size += 1 + String.valueOf(count).length();
+		return size;
 	}
 
 }
