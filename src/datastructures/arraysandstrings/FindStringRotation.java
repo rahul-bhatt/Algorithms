a35db555553774863278fed255dc32e40fd5b513
rahul-bhatt <rbhatt.27@gmail.com>
2018-06-10 11:52:59 +0530

    Find string rotation

--- src/datastructures/arraysandstrings/FindStringRotation.java
+++ src/datastructures/arraysandstrings/FindStringRotation.java
@@ -19,5 +19,30 @@ public class FindStringRotation {
 	public static void main(String[] args) {
 
 	}
+	
+	/**
+	 * Find if s1 is a rotation of s2.
+	 * 
+	 * Rotation of a string is based on the point of rotation. For example:
+	 * Given string "waterbottle" and rotation point is first "t", then
+	 * 
+	 * s2 = xy = waterbottle
+	 * x = wat, y = bottle
+	 * s1 = bottlewat (String after rotation)
+	 * 
+	 * Given this, if a string is rotated irrespective of the point of rotation
+	 * s1 would always be a substring of s2s2 i.e. xyxy
+	 * 
+	 * So the solution is simply to find if s1 is a substring of s2s2.
+	 */
+	public boolean isRotation(String s1, String s2) {
+		return isSubString(s1, s2 + s2);
+	}
+	
+	public boolean isSubString(String s1, String s2) {
+		// Given function.
+		// return statement for the sake of compilation.
+		return true;
+	}
 
 }
