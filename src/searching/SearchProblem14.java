2aa776b58e4a74b31523af64059924819c8aef98
rahul-bhatt <rbhatt.27@gmail.com>
2018-08-15 11:36:25 +0530

    search problem 14

--- src/searching/SearchProblem14.java
+++ src/searching/SearchProblem14.java
@@ -7,7 +7,13 @@ package searching;
  * @author rahulbhatt
  *
  * Problem: THE k'th LARGEST ELEMENT - LARGE n AND SMALL k
- * ******************************************************* 
+ * *******************************************************
+ * 
+ * Design an algorithm for computing the k-th largest element in a sequence (stream) of elements. It should run in O(n) expected time where 
+ * n is the length of the sequence, which is not known in advance. The value k is known in advance. It should use O(k) additional storage.
+ * 
+ * Solution:
+ * 
  */
 public class SearchProblem14 {
 
@@ -15,7 +21,6 @@ public class SearchProblem14 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
