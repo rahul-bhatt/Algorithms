bda3492e26c4c2f933fed37f5c39fd978929b286
rahul-bhatt <rbhatt.27@gmail.com>
2018-08-19 12:52:39 +0530

    search problem 5

--- src/searching/SearchProblem5.java
+++ src/searching/SearchProblem5.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package searching;
 
 /**
@@ -11,15 +8,42 @@ package searching;
  * 
  * Design an O(log n) algorithm for finding the position of the smallest element in a cyclically sorted array. Assume all elements are distinct.
  * 
+ * For example, for a cyclically sorted array - [4, 7, 8, 10, 12, 13, 15, 20, 1, 3], the output should return "8", which is the position of the
+ * smallest element "1".
+ * 
+ * Solution: Use binary search approach.
+ * 
+ * Time Complexity: O(log n).
+ * 
  */
 public class SearchProblem5 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] a = {4, 7, 8, 10, 12, 13, 15, 20, 1, 3};
+		System.out.println(findSmallest(a));
+		
+		int[] b = {387, 478, 550, 631, 103, 203, 220, 234, 279, 368};
+		System.out.println(findSmallest(b));
+		
+		int[] c = {0, 1, 2, 3, 4};
+		System.out.println(findSmallest(c));	}
+	
+	public static int findSmallest(int[] a) {
+		int left = 0;
+		int right = a.length - 1;
+		
+		while(a[left] > a[right]) {
+			int mid = left + (right - left) / 2;
+			if(left == mid) {
+				left = right;
+			} else if(a[mid] > a[left]) {
+				left = mid;
+			} else {
+				right = mid;
+			}
+		}
+		
+		return left;
 	}
 
 }
